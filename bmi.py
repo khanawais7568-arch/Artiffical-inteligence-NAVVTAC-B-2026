a6948085be2f2499e1be82f598a4cfe1df6aecd2
Muhammad Awais <khanawais7568@gmail.com>
2026-03-03 13:58:38 +0500

    Delete bmi.py

--- bmi.py
+++
@@ -1,8 +0,0 @@
-def calculate_bmi(weight, height):
-    return weight / (height ** 2)
-def bmi_menu():
-    while True:
-        w = float(input("Weight (kg): "))
-        h = float(input("Height (m): "))
-        b = calculate_bmi(w, h)
-        print("BMI:", round(b, 2))
